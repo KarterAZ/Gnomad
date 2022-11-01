c5eff766eb6cd647155317421b25e7e655816330
Bryce Schultz <bryceschultz@live.com>
2022-11-01 08:18:51 -0700

    pull request

--- react_design/src/components/map/Map.jsx
+++ react_design/src/components/map/Map.jsx
@@ -5,13 +5,13 @@ import './map.css'
 const key = process.env.REACT_APP_API_KEY
 
 const Map = ({ location, zoomLevel }) => (
-    <div className="map">
-      <GoogleMapReact
-          bootstrapURLKeys={{ key: key}}
-          defaultCenter={location}
-          defaultZoom={zoomLevel}
-        />
-    </div>
-  )
+  <div className="map">
+    <GoogleMapReact
+        bootstrapURLKeys={{ key: key}}
+        defaultCenter={location}
+        defaultZoom={zoomLevel}
+      />
+  </div>
+)
 
 export default Map
