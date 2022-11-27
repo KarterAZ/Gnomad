e7a32220b350bfbcd73d535c0f440c899209855a
Andrew Rice <aprspie@gmail.com>
2022-11-26 20:29:58 -0800

    navbar changes

--- frontend_app/travel-companion/src/components/topbar/Topbar.jsx
+++ frontend_app/travel-companion/src/components/topbar/Topbar.jsx
@@ -1,5 +1,12 @@
 export default function Topbar() {
   return (
-    <h1>Put navbar here?! (components/topbar)</h1>
+<ul class="navbar-nav">
+    <li class="nav-item">
+        <a class="nav-link text-dark" id="register" asp-area="Identity" asp-page="/Account/Register">Register</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link text-dark" id="login" asp-area="Identity" asp-page="/Account/Login">Login</a>
+    </li>
+</ul>
   );
 }
