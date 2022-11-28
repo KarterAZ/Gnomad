9e9c5bfb2481d2a8cf944e970e82053956f0cfb2
Andrew Rice <aprspie@gmail.com>
2022-11-28 15:05:29 -0800

    Layout to layout

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Home from './pages/home/Home';
 //import Layout from '/Areas/Identity/Pages/_Layout.cshtml'
 
+//layout should be Layout - need Layout first though
 function App() {
   return (
-    <Layout>
+    <layout>
       <Home/>
-    </Layout>
+    </layout>
   );
 }
 
