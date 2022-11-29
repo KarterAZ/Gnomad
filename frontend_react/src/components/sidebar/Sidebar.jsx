f4a15a3860cd2e737810b69f9d10c78a9e065756
Bryce Schultz <bryceschultz@live.com>
2022-11-29 14:33:33 -0800

    Modified the sidebar

--- frontend_react/src/components/sidebar/Sidebar.jsx
+++ frontend_react/src/components/sidebar/Sidebar.jsx
@@ -3,30 +3,33 @@ import './sidebar.css';
 export default function Sidebar()
 {
     return (
-        <div id="container">
+        <div id="sidebar-container">
             <div id="handle">
             </div>
 
-            <div id="content">
+            <div id="sidebar-content">
                 <section className="section" id="header-section">
                     <div id="user-section">
                         <a href="#">Register</a> <a href="#">Login</a>
                     </div>
 
                     <div id="settings-button-wrapper">
-                        <button id="settings-button">*</button>
+                        <button className="button" id="settings-button"></button>
                     </div>
                 </section>
 
                 <section className="section" id="search-section">
                     <label>Search:</label>
-                    <input type="text"></input>
-                    <button>Submit</button>
+                    <input id="search-bar" type="text"></input>
+                    <button className="button" id="search-button">Submit</button>
                 </section>
 
                 <section className="section" id="pins-section">
-                    <select></select>
-                    <button>Create</button>
+                    <div id="pin-group">
+                        <select id="pin-select"></select>
+                        <button className="button" id="pin-button">Create</button>
+                    </div>
+
                     <div id="pins-list">
 
                     </div>
