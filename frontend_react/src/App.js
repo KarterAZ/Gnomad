baf9d89d3a7a8428687e7f18724bda2a42d1cd79
Bryce Schultz <bryceschultz@live.com>
2022-11-29 14:06:06 -0800

    fixed last bit of stuff

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/home/Home';
-import Layout from './/layout-pages/Shared/_Layout.cshtml'
 
 //layout should be Layout - need Layout first though
 function App() {
   return (
-    <Layout>
       <Home/>
-    </Layout>
   );
 }
 
