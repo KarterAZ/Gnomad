93e4cdf9bb1023f3e62bed76519602a9c855e0c0
KarterAZ <kazwetschke@gmail.com>
2022-11-30 14:30:38 -0800

    added functions to interact with pins table

---
+++ backend_api/TravelCompanionAPI/Data/PinTableModifier.cs
@@ -0,0 +1,118 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelCompanionAPI.Models;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+using System.Data.Common;
+using System.Data;
+
+namespace TravelCompanionAPI.Data
+{
+    //******************************************************************************
+    //This class updates the Pins table, inheriting from IDataRepository.
+    //No new methods added.
+    //Implements getById, getAll, and add.
+    //******************************************************************************
+    public class PinTableModifier : IDataRepository<Pin>
+    {
+        const string TABLE = "pins";
+        private MySqlConnection _connection;
+        //Connection strings should be in secrets.json. Check out the resources tab in Discord to update yours (or ask Andrew).
+
+        public PinTableModifier(IConfiguration config)
+        {
+            //Switch depending on mode
+            string connection = null;
+            //connection = config.GetConnectionString("CodenomeDatabase");
+            connection = config.GetConnectionString("TestingDatabase");
+
+            _connection = new MySqlConnection(connection);
+        }
+
+        public Pin getById(int id)
+        {
+            Pin pins = null;
+            using (MySqlCommand command = new MySqlCommand())
+            {
+                command.Connection = _connection;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "SELECT * FROM + " + TABLE + " WHERE(`id` = @Id);";
+                command.Parameters.AddWithValue("Id", id);
+
+                _connection.Open();
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        pins = new Pin();
+                        pins.Id = reader.GetInt32(0);
+                        pins.Longitude = reader.GetInt32(1);
+                        pins.Latitude = reader.GetInt32(2);
+                        pins.Title = reader.GetString(3);
+                    }
+                }
+            }
+
+            _connection.Close();
+
+            return pins;
+        }
+
+        public List<Pin> getAll()
+        {
+            List<Pin> pins = new List<Pin>();
+
+            using (MySqlCommand command = new MySqlCommand())
+            {
+                command.Connection = _connection;
+                command.CommandType = CommandType.Text;
+                command.CommandText = @"SELECT * FROM " + TABLE + ";";
+
+                _connection.Open();
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Pin pin = new Pin();
+                        pin.Id = reader.GetInt32(0);
+                        pin.Longitude = reader.GetInt32(1);
+                        pin.Latitude = reader.GetInt32(2);
+                        pin.Title = reader.GetString(3);
+                        pins.Add(pin);
+                    }
+                }
+            }
+
+            _connection.Close();
+
+            return pins;
+        }
+
+        public int add(Pin pin)
+        {
+            using (MySqlCommand command = new MySqlCommand())
+            {
+                command.Connection = _connection;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "INSERT INTO " + TABLE + " (longitude, latitude, title) VALUES (@Longitude, @Latitude, @Title);";
+                command.Parameters.AddWithValue("@Longitude", pin.Longitude);
+                command.Parameters.AddWithValue("@DLatitude", pin.Latitude);
+                command.Parameters.AddWithValue("@Title", pin.Title);
+
+                _connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+
+            _connection.Close();
+
+            return 0;
+        }
+    }
+}
