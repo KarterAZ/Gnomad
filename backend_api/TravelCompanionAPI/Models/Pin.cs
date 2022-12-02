1226c0639046468f3a7edd28def2bc2a1ffa4351
KarterAZ <kazwetschke@gmail.com>
2022-12-01 17:27:55 -0800

    modified model for more pin functionality

--- backend_api/TravelCompanionAPI/Models/Pin.cs
+++ backend_api/TravelCompanionAPI/Models/Pin.cs
@@ -21,14 +21,17 @@ namespace TravelCompanionAPI.Models
         }
         [BindNever] //User shouldn't be able to change Id
         public int Id { get; set; }
+        public int UserId { get; set; }
         public int Longitude { get; set; }
         public int Latitude { get; set; }
         public string Title { get; set; }
+        public string Tag { get; set; }     //make pin collection class for multiple nearby pins
+        public string Street { get; set; }
 
         public void print()
         {
             Console.WriteLine(
-                "id: {0}\nlongitude: {1}\nlatitude name: {2}\ntitle: {3}", Id, Longitude, Latitude, Title);
+                "id: {0}\nlongitude: {1}\nlatitude name: {2}\ntitle: {3}\nstreet: {4}", Id, Longitude, Latitude, Title, Street);
         }
     }
 }
