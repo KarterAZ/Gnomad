3a1cfc7f08709091e27b96ace171db4ad058621e
Bryce Schultz <bryceschultz@live.com>
2022-12-03 20:10:31 -0800

    reformatted some files

--- frontend_react/src/components/sidebar/Sidebar.jsx
+++ frontend_react/src/components/sidebar/Sidebar.jsx
@@ -4,23 +4,19 @@ import { LoginButton, LogoutButton } from '../login_button/LoginButton';
 
 import './sidebar.css';
 
-
 class Sidebar extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.handleClick = this.handleClick.bind(this);
     this.open = true;
-
   }
 
   handleClick() {
     if (this.open) {
-
-      document.getElementById("sidebar-container").style.width = "0px";
-    }
-    else {
-      document.getElementById("sidebar-container").style.width = "35%";
+      document.getElementById('sidebar-container').style.width = '0px';
+    } else {
+      document.getElementById('sidebar-container').style.width = '35%';
     }
 
     this.open = !this.open;
@@ -28,45 +24,49 @@ class Sidebar extends Component {
 
   render() {
     return (
-      <div id="sidebar-wrapper">
-        <div id="sidebar-container">
-          <div id="sidebar-content">
-            <section className="section" id="header-section">
-              <div id="user-section">
+      <div id='sidebar-wrapper'>
+        <div id='sidebar-container'>
+          <div id='sidebar-content'>
+            <section className='section' id='header-section'>
+              <div id='user-section'>
                 <LoginButton />
                 <LogoutButton />
               </div>
 
-              <div id="settings-button-wrapper">
-                <Icon id="settings-button" icon="ph:gear-six-duotone" />
+              <div id='settings-button-wrapper'>
+                <Icon id='settings-button' icon='ph:gear-six-duotone' />
               </div>
             </section>
 
-            <section className="section" id="search-section">
+            <section className='section' id='search-section'>
               <label>Search:</label>
-              <input id="search-bar" type="text"></input>
-              <button className="button" id="search-button">Submit</button>
+              <input id='search-bar' type='text'></input>
+              <button className='button' id='search-button'>
+                Submit
+              </button>
             </section>
 
-            <section className="section" id="pins-section">
-              <div id="pin-group">
-                <select defaultValue={0} id="pin-select">
-                  <option value="0" disabled>Select a Pin Type</option>
-                  <option value="1">All</option>
-                  <option value="2">Bathrooms</option>
-                  <option value="3">Wi-Fi</option>
+            <section className='section' id='pins-section'>
+              <div id='pin-group'>
+                <select defaultValue={0} id='pin-select'>
+                  <option value='0' disabled>
+                    Select a Pin Type
+                  </option>
+                  <option value='1'>All</option>
+                  <option value='2'>Bathrooms</option>
+                  <option value='3'>Wi-Fi</option>
                 </select>
-                <button className="button" id="pin-button">Create</button>
+                <button className='button' id='pin-button'>
+                  Create
+                </button>
               </div>
 
-              <div id="pins-list">
-
-              </div>
+              <div id='pins-list'></div>
             </section>
           </div>
         </div>
-        <div onClick={this.handleClick} id="handle">
-          <Icon icon="charm:menu-hamburger" />
+        <div onClick={this.handleClick} id='handle'>
+          <Icon icon='charm:menu-hamburger' />
         </div>
       </div>
     );
@@ -74,3 +74,4 @@ class Sidebar extends Component {
 }
 
 export default Sidebar;
+
