2e588f5d08a6937ceb8afc53d3602af2f000be56
Bryce Schultz <bryceschultz@live.com>
2022-12-03 19:53:22 -0800

    added google login

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/home/Home';
 
-//layout should be Layout - need Layout first though
 function App() {
   return (
     <Router>
-    <Routes>
-        <Route exact path='/' element={<Home/>} />
-    </Routes>
+      <Routes>
+          <Route exact path='/' element={<Home/>} />
+      </Routes>
     </Router>
   );
 }
--- frontend_react/src/components/sidebar/Sidebar.jsx
+++ frontend_react/src/components/sidebar/Sidebar.jsx
@@ -5,78 +5,72 @@ import { LoginButton, LogoutButton } from '../login_button/LoginButton';
 import './sidebar.css';
 
 
-class Sidebar extends Component
-{
-    constructor(props)
-    {
-        super(props)
+class Sidebar extends Component {
+  constructor(props) {
+    super(props)
 
-        this.handleClick = this.handleClick.bind(this);
-        this.open = true;
-        
+    this.handleClick = this.handleClick.bind(this);
+    this.open = true;
+
+  }
+
+  handleClick() {
+    if (this.open) {
+
+      document.getElementById("sidebar-container").style.width = "0px";
+    }
+    else {
+      document.getElementById("sidebar-container").style.width = "35%";
     }
 
-    handleClick()
-    {
-        if (this.open)
-        {
-            
-            document.getElementById("sidebar-container").style.width = "0px";
-        }
-        else
-        {
-            document.getElementById("sidebar-container").style.width = "35%";
-        }
+    this.open = !this.open;
+  }
 
-        this.open = !this.open;
-    }
+  render() {
+    return (
+      <div id="sidebar-wrapper">
+        <div id="sidebar-container">
+          <div id="sidebar-content">
+            <section className="section" id="header-section">
+              <div id="user-section">
+                <LoginButton />
+                <LogoutButton />
+              </div>
 
-    render() 
-    {
-        return (
-            <div id="sidebar-wrapper">
-                <div id="sidebar-container">
-                    <div id="sidebar-content">
-                        <section className="section" id="header-section">
-                            <div id="user-section">
-                                <LoginButton/>
-                                <LogoutButton/>
-                            </div>
+              <div id="settings-button-wrapper">
+                <Icon id="settings-button" icon="ph:gear-six-duotone" />
+              </div>
+            </section>
 
-                            <div id="settings-button-wrapper">
-                                <Icon id="settings-button" icon="ph:gear-six-duotone"/>
-                            </div>
-                        </section>
+            <section className="section" id="search-section">
+              <label>Search:</label>
+              <input id="search-bar" type="text"></input>
+              <button className="button" id="search-button">Submit</button>
+            </section>
 
-                        <section className="section" id="search-section">
-                            <label>Search:</label>
-                            <input id="search-bar" type="text"></input>
-                            <button className="button" id="search-button">Submit</button>
-                        </section>
+            <section className="section" id="pins-section">
+              <div id="pin-group">
+                <select defaultValue={0} id="pin-select">
+                  <option value="0" disabled>Select a Pin Type</option>
+                  <option value="1">All</option>
+                  <option value="2">Bathrooms</option>
+                  <option value="3">Wi-Fi</option>
+                </select>
+                <button className="button" id="pin-button">Create</button>
+              </div>
 
-                        <section className="section" id="pins-section">
-                            <div id="pin-group">
-                                <select defaultValue={0} id="pin-select">
-                                    <option value="0" disabled>Select a Pin Type</option>
-                                    <option value="1">All</option>
-                                    <option value="2">Bathrooms</option>
-                                    <option value="3">Wi-Fi</option>
-                                </select>
-                                <button className="button" id="pin-button">Create</button>
-                            </div>
+              <div id="pins-list">
 
-                            <div id="pins-list">
-
-                            </div>
-                        </section>
-                    </div>
-                </div>
-                <div onClick={this.handleClick} id="handle">
-                    <Icon icon="charm:menu-hamburger"/>
-                </div>
-            </div>
-        );
-    }
+              </div>
+            </section>
+          </div>
+        </div>
+        <div onClick={this.handleClick} id="handle">
+          <Icon icon="charm:menu-hamburger" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Sidebar;
