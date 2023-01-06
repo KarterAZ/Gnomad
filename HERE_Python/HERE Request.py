52830f1260aa82dfd5c2f794fb68d54384670768
Stephen-Thomson <shimmergloom6@yahoo.com>
2023-01-06 11:25:35 -0800

    Working out errors

--- HERE_Python/HERE Request.py
+++ HERE_Python/HERE Request.py
@@ -1,5 +1,3 @@
-Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 import requests
 import json
 
@@ -12,7 +10,7 @@ api_key = 'LbzvmA77Y0eEWFQAjTp-LLpUWmISGj3XWe0rRD4Wei4'
 params = {
     "at": "42.2249,-121.7817",  #Center of Klamath Falls, Oregon
     "q": "bathroom",  #Category Search
-    "radius": 25  #Radius of search from center point
+    "radius": 100  #Radius of search from center point
 }
 
 #Set the API key for authentication
@@ -22,7 +20,9 @@ headers = {
 
 # Send the request and retrieve the response
 response = requests.get(endpoint, params=params, headers=headers)
- 
+
+print("Status code:", response.status_code)
+
 # Check the status code of the response
 if response.status_code != 200:
     print("Error: API request failed")
---
+++ HERE_Python/TestingHere2.py
@@ -0,0 +1,60 @@
+from sys import argv, stderr, exit
+from json import dumps
+import logging
+import http.client
+http.client.HTTPConnection.debuglevel = 1
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger('requests.packages.urllib3')
+requests_log.propagate = True
+requests_log.setLevel(logging.DEBUG)
+
+try:
+    from requests_oauthlib import OAuth1
+except ImportError as e:
+    raise ImportError(f'{e.msg}\nPackage available at https://pypi.org/project/requests-oauthlib')
+
+try:
+    from requests import get, post, HTTPError
+except ImportError as e:
+    raise ImportError(f'{e.msg}\nPackage available at https://pypi.org/project/requests')
+
+usage = """Usage:
+    hgs_access_test.py <key-id> <key_secret>
+"""
+clid = "7rCT77hOpd9VBv8K0DAxag"
+clsec = "OyOPKetreHVGT5Ry5koisPp84dI4OjD7wTNrHYDsSllkk54-P5_Blkq2KobpEA6QY0BPvkqyyBClzrS7xtU90w"
+
+search_query = 'https://discover.search.hereapi.com/v1/discover?in=circle:42.2249,-121.7817;r=115000&q=bathroom&apiKey=LbzvmA77Y0eEWFQAjTp-LLpUWmISGj3XWe0rRD4Wei4'
+
+# 1. Retrieve token
+try:
+    data = {
+        'grantType': 'client_credentials',
+        'clientId': clid,
+        'clientSecret': clsec
+        }
+except IndexError:
+    stderr.write(usage)
+    exit(1)
+
+response = post(
+    url='https://account.api.here.com/oauth2/token',
+    auth=OAuth1(clid, client_secret=clsec) ,
+    headers= {'Content-type': 'application/json'},
+    data=dumps(data)).json()
+
+try:
+    token = response['accessToken']
+    token_type = response['tokenType']
+    expire_in = response['expiresIn']
+except KeyError as e:
+    print(dumps(response, indent=2))
+    exit(1)
+
+# 2. Use it in HERE Geocoding And Search query header
+headers = {'Authorization': f'{token_type} {token}'}
+search_results = dumps(get(search_query, headers=headers).json(), indent=2)
+
+print(f'results:\n{search_results}')
