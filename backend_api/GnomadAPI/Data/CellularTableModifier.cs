382cdfe4278476587108fe343c29cc8a29a5b6c6
KarterAZ <kazwetschke@gmail.com>
2023-01-20 22:43:32 -0800

    started adding class and modifiers for cellular data

---
+++ backend_api/GnomadAPI/Data/CellularTableModifier.cs
@@ -0,0 +1,128 @@
+﻿/************************************************************************************************
+*
+* Author: Bryce Schultz, Andrew Rice, Karter Zwetschke, Andrew Ramirez, Stephen Thomson
+* Date: 12/28/2022
+*
+* Purpose: Holds the functions for table access.
+*
+************************************************************************************************/
+
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelCompanionAPI.Models;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+using System.Data.Common;
+using System.Data;
+
+namespace TravelCompanionAPI.Data
+{
+    //******************************************************************************
+    //This class updates the h3_oregon_data table, inheriting from IDataRepository.
+    //No new methods added.
+    //Implements getById, getAll
+    //******************************************************************************
+    public class CellularTableModifier : IDataRepository<Cellular>
+    {
+        const string TABLE = "h3_oregon_data";
+        private MySqlConnection _connection;
+        //Connection strings should be in secrets.json. Check out the resources tab in Discord to update yours (or ask Andrew).
+
+        public CellularTableModifier(IConfiguration config)
+        {
+            //Switch depending on mode
+            string connection = null;
+            //connection = config.GetConnectionString("CodenomeDatabase");
+            connection = config.GetConnectionString("TestingDatabase");
+
+            _connection = new MySqlConnection(connection);
+        }
+
+        public Cellular getById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cellular getByH3Id(string id)
+        {
+            Cellular cellular = null;
+            using (MySqlCommand command = new MySqlCommand())
+            {
+                command.Connection = _connection;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "SELECT * FROM + " + TABLE + " WHERE(`h3_res9_id` = @Id);";
+                command.Parameters.AddWithValue("Id", id);
+
+                _connection.Open();
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        cellular = new Cellular();
+                        cellular.Technology = reader.GetInt32(0);
+                        cellular.Mindown = reader.GetInt32(1);
+                        cellular.Minup = reader.GetInt32(2);
+                        cellular.Environment = reader.GetInt32(3);
+                        cellular.H3id = reader.GetString(4);
+                    }
+                }
+            }
+
+            _connection.Close();
+
+            return cellular;
+        }
+
+        public List<Cellular> getAll()
+        {
+            List<Cellular> h3_oregon_data = new List<Cellular>();
+
+            using (MySqlCommand command = new MySqlCommand())
+            {
+                command.Connection = _connection;
+                command.CommandType = CommandType.Text;
+                command.CommandText = @"SELECT * FROM " + TABLE + ";";
+
+                _connection.Open();
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Cellular cellular = new Cellular();
+                        cellular.Technology = reader.GetInt32(0);
+                        cellular.Mindown = reader.GetInt32(1);
+                        cellular.Minup = reader.GetInt32(2);
+                        cellular.Environment = reader.GetInt32(3);
+                        cellular.H3id = reader.GetString(4);
+                        h3_oregon_data.Add(cellular);
+                    }
+                }
+            }
+
+            _connection.Close();
+
+            return h3_oregon_data;
+        }
+
+        public bool add(Cellular cellular)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool contains(Cellular data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cellular> getAllByUser(int uid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ backend_api/GnomadAPI/Models/Cellular.cs
@@ -0,0 +1,45 @@
+﻿/************************************************************************************************
+*
+* Author: Bryce Schultz, Andrew Rice, Karter Zwetschke, Andrew Ramirez, Stephen Thomson
+* Date: 11/28/2022
+*
+* Purpose: Class definition for cellular data
+*
+************************************************************************************************/
+
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelCompanionAPI.Data;
+
+namespace TravelCompanionAPI.Models
+{
+    public class Cellular : IDataEntity
+    {
+        public Cellular()
+        { }
+
+        public Cellular(int technology, int mindown, int minup, int environment, string h3id)
+        {
+            Technology = technology;
+            Mindown = mindown;
+            Minup = minup;
+            Environment = environment;
+            H3id = h3id;
+        }
+
+        public int Technology { get; set; }
+        public int Minup { get; set; }
+        public int Mindown { get; set; }
+        public int Environment { get; set; }
+        public string H3id { get; set; }
+
+        public void print()
+        {
+            Console.WriteLine(
+                "Technology: {0}\nMindown: {1}\nMinup: {2}\nEnvironment: {3}\nH3id: {4}\n", Technology, Mindown, Minup, Environment, H3id);
+        }
+    }
+}
