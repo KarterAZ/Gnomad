053b76f003372bc063b9ac6d6a3cd2063ab68f32
Andrew Rice <aprspie@gmail.com>
2023-01-29 10:03:12 -0800

    Added gas script

--- backend_api/GnomadAPI/Fuel/AddGasPins/AddGasPins.cs
+++ backend_api/GnomadAPI/Fuel/AddGasPins/AddGasPins.cs
@@ -1,38 +1,39 @@
 //Script to add supercharger pins to the database.
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 using TravelCompanionAPI.Data;
 using TravelCompanionAPI.Models;
 
 namespace TravelCompanionAPI.Fuel
 {
-
-    static class AddingSuperchargerData
+    static class AddingGasData
     {
         //Adds the relevent data to the database.
-        public static void AddSuperchargers(IDataRepository<Pin> pin_repo)
+        public static void AddGas(IDataRepository<Pin> pin_repo)
         {
             //Get connection string and set up database done in pin_repo (of type PinTableModifier)
             //File I/O
-            string path = Path.Join(Directory.GetCurrentDirectory(), "\\Fuel\\AddSuperchargerPins\\SuperchargerData.txt");
+            string path = Path.Join(Directory.GetCurrentDirectory(), "\\Fuel\\AddGasPins\\GasData.txt");
 
             if (File.Exists(path)) //Only run if file is found (should be found)
             {
-                foreach (SuperchargerData data in JsonSerializer.Deserialize<List<SuperchargerData>>(File.ReadAllText(path)))
+                foreach (GasData data in JsonSerializer.Deserialize<List<GasData>>(File.ReadAllText(path)))
                 {
-                    if (data.address.country.Equals("USA") && data.status.Equals("OPEN")) //If an open us supercharger
-                    {
                         Pin pin = new Pin();
                         pin.UserId = 0;
-                        pin.Longitude = data.gps.longitude ?? 0;
-                        pin.Latitude = data.gps.latitude ?? 0;
-                        pin.Title = data.name + " Supercharger";
-                        pin.Street = data.address.street;
-                        pin.Tags.Add((int)TagValues.tags.TeslaSupercharger);
+                        pin.Longitude = data.geometry.coordinates.Last();
+                        pin.Latitude = data.geometry.coordinates.First();
+                        pin.Title = data.properties.name;
+                        pin.Street = data.properties.addr_housenumber + " " + data.properties.addr_street;
+
+                        if (data.properties.fuel_gasoline == "yes")
+                            pin.Tags.Add((int)TagValues.tags.Regular);
+                        if (data.properties.fuel_diesel == "yes")
+                            pin.Tags.Add((int)TagValues.tags.Diesel);
 
                         pin_repo.add(pin);
-                    }
                 }
             }
         }
--- backend_api/GnomadAPI/Fuel/AddGasPins/GasData.cs
+++ backend_api/GnomadAPI/Fuel/AddGasPins/GasData.cs
@@ -1,42 +1,88 @@
-﻿//Script to add supercharger pins to the database.
+﻿//Script to add gas pins to the database.
 using System.Collections.Generic;
 
 
 //Generic layout of JSON
-public class SuperchargerData
+public class GasData
 {
-    public int? id { get; set; }
-    public string? locationId { get; set; } //Possible name
-    public string? name { get; set; } //Possible name (used below)
-    public string? status { get; set; } //Only use data if !strcmp(status, "OPEN")
-    //public Dictionary<string, AddressData> address { get; set; }
-    public AddressData address { get; set; } //Useful. Address and only use data if !strcmp(address["country"], "USA".
-    public LatitudeAndLongitude gps { get; set; } //Useful.
-    public string? dateOpened { get; set; }
-    public int? stallCount { get; set; } //Possibly nice, not for now
-    public bool? counted { get; set; }
-    public int? elevationMeters { get; set; }
-    public int? powerKilowatt { get; set; }
-    public bool? solarCanopy { get; set; }
-    public bool? battery { get; set; }
-    public int? statusDays { get; set; }
-    public bool? urlDiscuss { get; set; }
+    public string? type { get; set; } //Always Feature? Doesn't seem useful.
+    public Properties properties { get; set; } //Useful, has type of gas
+    public Geometry geometry { get; set; } //Useful, lat and long
+    public string? id { get; set; }
 }
 //Layout of gps data member in JSON
-public class LatitudeAndLongitude
+public class Geometry
 {
-    public double? latitude { get; set; }
-    public double? longitude { get; set; }
+    public string? type { get; set; }
+    public List<int> coordinates { get; set; } //Stored latitude, longitude
 }
 //Layout of address data member in JSON
-public class AddressData
+public class Properties
 {
-    public string? street { get; set; }
-    public string? city { get; set; }
-    public string? state { get; set; }
-    public string? zip { get; set; }
-    public int? countryId { get; set; }
-    public string? country { get; set; }
-    public int? regionId { get; set; }
-    public string? region { get; set; }
+    public string? pid { get; set;}
+    public string? addr_city { get; set;} //Potentially useful
+    public string? addr_housenumber { get; set;} //Useful
+    public string? addr_postcode { get; set;}
+    public string? addr_state { get; set;} //Potentially useful
+    public string? addr_street { get; set;} //Useful
+    public string? amenity { get; set;} //Should be fuel
+    public string? brand { get; set;} //Potentialy useful
+    public string? brand_wikidata { get; set;}
+    public string? brand_wikipedia { get; set;}
+    public string? census_population { get; set; }
+    public string? cuisine { get; set; }
+    public string? ele { get; set; }
+    public string? fuel_biodiesel { get; set; } //Useful
+    public string? fuel_diesel { get; set;} //Useful
+    public string? fuel_gasoline { get; set;} //Useful
+    public string? fuel_octane_85 { get; set;}
+    public string? fuel_octane_87 { get; set;}
+    public string? fuel_octane_91 { get; set;}
+    public string? fuel_e10 { get; set;} //Ethanol 10
+    public string? fuel_lpg { get; set;} //liquefied petroleum
+    public string? fuel_LH2 { get; set;} //liqid hydrogen
+    public string? gnis_Class { get; set; }
+    public string? gnis_County { get; set; }
+    public string? gnis_County_num { get; set; }
+    public string? gnis_ST_alpha { get; set; }
+    public string? gnis_id { get; set; }
+    public string? gnis_ST_num { get; set; }
+    public string? import_uuid { get; set; }
+    public string? name { get; set;} //Useful
+    public string? opening_hours { get; set;} //Potentially useful
+    public string? phone { get; set;} //Potentially useful
+    public string? self_service { get; set;}
+    public string? website { get; set; } //Potentially useful
+    public string? payment_cards { get; set; }
+    public string? payment_cash { get; set; }
+    public string? payment_credit_cards { get; set; }
+    public string? source { get; set; }
+    public string? source_ref { get; set; }
+    public string? population { get; set; }
 }
+
+/*
+amenity=fuel
+brand=CONCH
+
+operator=BigOil Inc.
+
+name=Classic
+tenant=John Doe
+opening_hours=Mo-Fr 05:30-23:00; Sa 07:00-21:00; Su off
+payment:cash=yes
+payment:mastercard=yes
+payment:visa=yes
+payment:diners_club=no
+payment:american_express=yes
+payment:maestro=yes
+payment:dkv=yes
+payment:uta=yes
+fuel:diesel=yes
+fuel:octane_91=yes
+fuel:octane_95=yes
+fuel:octane_98=yes
+fuel:e10=yes
+fuel:lpg=yes
+fuel:LH2=yes
+*/
