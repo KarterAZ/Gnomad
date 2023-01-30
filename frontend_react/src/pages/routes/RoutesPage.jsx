1822f8d38b304ed014f6b2676140db076f5d0ff5
Bryce Schultz <bryceschultz@live.com>
2023-01-30 14:37:50 -0800

    Create route page (#37)
    * started work on the create routes page
    
    * Changed open to useState in Sidebar
    
    * fixed the sizing on the routes page
    
    * changed the sidebar a bit
    
    * changed more sidebar stuff
    
    * changed the login button again...

--- frontend_react/src/pages/routes/RoutesPage.jsx
+++ frontend_react/src/pages/routes/RoutesPage.jsx
@@ -1,3 +1,9 @@
+// external imports.
+import { Icon } from '@iconify/react';
+import roundKeyboardArrowUp from '@iconify/icons-ic/round-keyboard-arrow-up';
+import roundKeyboardArrowDown from '@iconify/icons-ic/round-keyboard-arrow-down';
+import baselineSearch from '@iconify/icons-ic/baseline-search';
+
 // internal imports.
 import './routes.css';
 
@@ -5,7 +11,54 @@ import './routes.css';
 export default function RoutesPage()
 {
     return(
-        <>
-        </>
+        <div id='centered-content'>
+            <div id='form'>
+                <div id='form-header'>
+                    <h1>Create a Route</h1>
+                </div>
+                <div id='form-body'>
+                    <div id='padded-wrapper'>
+                        <div className='input-section-wrapper'>
+                            <label>Route Name:</label>
+                            <input className='text-input' type='text'></input>
+                        </div>
+                        <div className='input-section-wrapper'>
+                            <label>Search for Pins:</label>
+                            <div className='search-wrapper'>
+                                <input className='text-input' type='text'></input>
+                                <button className='search-button'><Icon icon={baselineSearch} width="20" height="20"/></button>
+                            </div>
+                        </div>
+                        <div className='input-section-wrapper max-height'>
+                            <div className='multi-select'>
+
+                            </div>
+                        </div>
+
+                        <div className='input-section-wrapper'>
+                            <div id='add-remove-buttons'>
+                                <button className='small-button'><Icon icon={roundKeyboardArrowDown} width="30" height="30" /></button>
+                                <div id='small-button-spacer'></div>
+                                <button className='small-button'><Icon icon={roundKeyboardArrowUp} width="30" height="30" /></button>
+                            </div>
+                        </div>
+
+                        <div className='input-section-wrapper'>
+                            <label>Search for Pins:</label>
+                            <div className='search-wrapper'>
+                                <input className='text-input' type='text'></input>
+                                <button className='search-button'><Icon icon={baselineSearch} width="20" height="20"/></button>
+                            </div>
+                        </div>
+
+                        <div className='input-section-wrapper max-height'>
+                            <div className='multi-select'>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
