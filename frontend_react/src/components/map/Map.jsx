7a9873f77506ff79c731ad6b7830b6a30a93cc29
LetsRollDrew <andrew.toolsfs@gmail.com>
2023-01-29 22:27:24 -0800

    working map, marker array blueprint

--- frontend_react/src/components/map/Map.jsx
+++ frontend_react/src/components/map/Map.jsx
@@ -8,96 +8,70 @@
 //################################################################
 
 import React, { Component, useState, useRef, useEffect } from 'react';
-import {
-  GoogleMapsProvider,
-  useGoogleMap,
-} from "@ubilabs/google-maps-react-hooks";
+
+import GoogleMapReact from 'google-map-react';
 
 // internal imports.
 import './map.css';
 import './markers.css';
 import pin from './pin.png';
 
-
-const defaultProps = {
-  zoom: 12,
-  center: {
-    lat: 42.2565,
-    lng: -121.78,
-  },
-  
-};
-
-/** 
-const Marker = ({ lat, lng }) => (
-
-  <img src={pin} alt="pin" style={{
-    position: 'absolute',
-    transform: `translate(${-20 / 2}px,${-40}px)`,
-    width: '80px',
-    height: '70px',
-    text: "Sample Text",
-
-  }}
-    lat={lat}
-    lng={lng}
-  />
-);
-*/
-
-
 export default function Map() {
-  const [mapContainer, setMapContainer] = useState(null);
 
-  return (
-    <GoogleMapsProvider
-      googleMapsAPIKey={'AIzaSyCHOIzfsDzudB0Zlw5YnxLpjXQvwPmTI2o'}
-      options={defaultProps}
-      mapContainer={mapContainer}
-    >
-      <div ref={(node) => setMapContainer(node)} style={{ height: "100vh", width: "100%",}} />
-      
-    </GoogleMapsProvider>
-  );
-}
+  const defaultProps = {
+    zoom: 12,
+    center: {
+      lat: 42.2565,
+      lng: -121.78,
+    },
+    
+  };
 
-/** 
-function Location() {
-  const [lat, setLat] = useState(43.68);
-  const [lng, setLng] = useState(-79.43);
-  const { map } = useGoogleMap();
-  const markerRef = useRef();
-
-  useEffect(() => {
-    if (!map || markerRef.current) return;
-    markerRef.current = new window.google.maps.Marker({ map });
-  }, [map]);
-
-  useEffect(() => {
-    if (!markerRef.current) return;
-    if (isNaN(lat) || isNaN(lng)) return;
-    markerRef.current.setPosition({ lat, lng });
-    map.panTo({ lat, lng });
-  }, [lat, lng, map]);
-
-  return (
-    <div className="lat-lng">
-      <input
-        type="number"
-        value={lat}
-        onChange={(event) => setLat(parseFloat(event.target.value))}
-        step={0.01}
+  const Marker = ({ lat, lng }) => (
+    <img src={pin} alt="pin" style={{
+      position: 'absolute',
+      transform: `translate(${-20 / 2}px,${-40}px)`,
+      width: '80px',
+      height: '70px',
+      text: "Sample Text",
+    }}
+      lat={lat}
+      lng={lng}
       />
-      <input
-        type="number"
-        value={lng}
-        onChange={(event) => setLng(parseFloat(event.target.value))}
-        step={0.01}
-      />
+   );
+   
+    /*
+    const[markers,setMarkers]=React.useState([]);
+    React.useEffect(() => {
+      const map = document.getElementById('map').childNodes[0].childNodes[0]._googleMapComponent.map;
+      GoogleMapReact.maps.event.addListener(map, 'click', (event) => {
+        setMarkers(current => [...current, {
+          lat: event.latLng.lat(),
+          lng: event.latLng.lng(),
+        }]);
+      });
+    }, []);
+    **/
+    return (
+    <div id='map'>
+    <div id='wrapper'>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyCHOIzfsDzudB0Zlw5YnxLpjXQvwPmTI2o' }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <Marker
+              lat={42.255}
+              lng={-121.7855}
+              text="Oregon Tech"
+            />
+        
+      </GoogleMapReact>
     </div>
+  </div>
   );
 }
-*/
+
 //Rough mockup of what we can do for varying pin images, modified from an example 
 
 /** const Pin = ({  pin }) => {
