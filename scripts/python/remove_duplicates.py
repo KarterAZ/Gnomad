063e1ae3aeaa59edb7b0d6243ce5fe4b8cb861b3
Stephen-Thomson <shimmergloom6@yahoo.com>
2023-02-09 19:29:06 -0800

    Minor Python script changes

--- scripts/python/remove_duplicates.py
+++ scripts/python/remove_duplicates.py
@@ -1,17 +1,17 @@
-# Author: Stephen Thomson, Bryce Schultz, Andrew Rice, Karter Zwetschke, Andrew Ramirez
-# Date: 1/7/2023
-# Purpose: Breaks Oregon into 2 circles with a 250km radius to theoretically cover all of Oregon.
-#           Sends 2 requests to the HERE API, parses the bathroom data we need, and inserts it into the Testing Database.
+# Author: Stephen Thomson
+# Date: 2/3/2023
+# Purpose: Goes through the pins table, finds repeat entries, and removes the duplicates
+#           of both wifi and bathroom tags.
 
-import pymysql
+import mysql.connector
 
 # Connect to the database
-connection = pymysql.connect(host='travel.bryceshultz.com',
-                             user='codenome',
-                             password='Codenome!1',
-                             db='codenome_testing')
+connection = mysql.connector.connect(user='codenome', password='Codenome!1',
+                              host='travel.bryceschultz.com', database='codenome_testing')
+
 
 try:
+    #Run through bathroom tags
     with connection.cursor() as cursor:
         # Select rows with duplicate latitude and longitude values
         sql = """
@@ -28,7 +28,7 @@ try:
         for duplicate in duplicates:
             latitude, longitude, count = duplicate
             sql = f"""
-                DELETE FROM your_table
+                DELETE FROM pins
                 WHERE latitude = {latitude} AND longitude = {longitude} AND tag_bathroom = 1
                 LIMIT {count-1};
             """
@@ -37,6 +37,31 @@ try:
     # Commit the changes
     connection.commit()
 
+    #Run through wifi tags
+    with connection.cursor() as cursor:
+        # Select rows with duplicate latitude and longitude values
+        sql = """
+            SELECT latitude, longitude, COUNT(*)
+            FROM pins
+            WHERE tag_wifi = 1
+            GROUP BY latitude, longitude
+            HAVING COUNT(*) > 1;
+        """
+        cursor.execute(sql)
+        duplicates = cursor.fetchall()
+
+        # Remove the duplicate rows
+        for duplicate in duplicates:
+            latitude, longitude, count = duplicate
+            sql = f"""
+                DELETE FROM pins
+                WHERE latitude = {latitude} AND longitude = {longitude} AND tag_wifi = 1
+                LIMIT {count-1};
+            """
+            cursor.execute(sql)
+
+    # Commit the changes
+    connection.commit()
 finally:
     # Close the connection
     connection.close()
