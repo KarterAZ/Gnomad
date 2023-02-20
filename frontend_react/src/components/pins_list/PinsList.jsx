a036f68a29768ad6d004ea18809f5565e0313e44
Bryce Schultz <bryceschultz@live.com>
2023-02-19 19:21:43 -0800

    Route page logic (#46)
    * fixed package.json
    
    * started work on the PinsList component
    
    * added the pin data object
    
    * added styling for the pins list
    
    * working on getting pins from global pins list into routes pin list
    
    * added more functionality

--- frontend_react/src/components/pins_list/PinsList.jsx
+++ frontend_react/src/components/pins_list/PinsList.jsx
@@ -1,8 +1,51 @@
-export default function PinsList()
+import { useEffect, useState } from "react";
+import ListElement from "./ListElement";
+import Pin from "../../data/pin";
+import "./pins_list.css";
+
+export default function PinsList({
+    pins,
+    onClick
+})
 {
+    let list_items = pins.map(
+        (item) => 
+        <ListElement pin={item} key={item.id} onClick={clicked}></ListElement>
+    );
+
+    const clicked = (pin, active) =>
+    {
+        onClick(pin, active);
+    }
+
+    /*
+    const addPin = (pin) =>
+    {
+        setPins([
+            ...pins,
+            pin
+        ]);
+    }
+
+    const removePin = (pin) =>
+    {
+        setPins(
+            pins.filter(p => p.id !== pin.id)
+        );
+    }
+    */
+
+    useEffect(() =>
+    {
+        let pin = new Pin(0, 0, 10, 20, 'Pin Title', 'Street', []);
+        //setPins([pin, pin, pin, pin, pin, pin, pin]);
+    }, []);
 
     return (
-        <>
-        </>
+        <div className='multi-select'>
+            <ul className='list'>
+                { list_items }
+            </ul>
+        </div>
     );
 }
