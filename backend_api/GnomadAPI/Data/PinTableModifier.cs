020e6bd2ac98df12b753a766eb8f9034728fe928
Andrew Rice <aprspie@gmail.com>
2023-02-26 19:47:05 -0800

    Got the function written to GetAllByTag

--- backend_api/GnomadAPI/Data/PinTableModifier.cs
+++ backend_api/GnomadAPI/Data/PinTableModifier.cs
@@ -356,12 +356,30 @@ namespace TravelCompanionAPI.Data
         List<Pin> GetAllByTag(List<int> tags)
         {
             List<Pin> pins = new List<Pin>();
+            List<Pin> deleteList = new List<Pin>();
             pins = this.getAll();
 
-            //Foreach loops
+            foreach(Pin pin in pins)
+            {
+                bool delete = true;
+                foreach(int tag in tags)
+                {
+                    if(delete)
+                    {
+                        if(pin.Tags.Contains(tag))
+                        {
+                            delete = false;
+                        }
+                    }
+                }
+                if(delete)
+                {
+                    deleteList.Add(pin);
+                }
+            }
 
-            //Remove all pins outside of range
-            pins.RemoveAll(pin => (!pin.Tags.Contains(0)));
+            //Remove all pins without the specified tags
+            pins.RemoveAll(pin => (deleteList.Contains(pin)));
 
             return pins;
         }
