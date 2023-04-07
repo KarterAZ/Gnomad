0ab8de27425fc9bae46c4254fca8815404441a21
KarterAZ <kazwetschke@gmail.com>
2023-04-06 23:51:38 -0700

    added script to make coords database

---
+++ scripts/sql/create/create_cell_coords_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `codenome_db`.`cell_coords` (
+  `center` INT NOT NULL,
+  `v1` INT NOT NULL,
+  `v2` INT NOT NULL,
+  `v3` INT NOT NULL,
+  `v4` INT NOT NULL,
+  `v5` INT NOT NULL,
+  `v6` INT,
+  PRIMARY KEY (`center`));
