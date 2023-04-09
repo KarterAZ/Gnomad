52b4db06e54c0b59ef92e9444cc2f3f610a1d322
KarterAZ <kazwetschke@gmail.com>
2023-04-08 22:20:30 -0700

    removing duplicate stuff

--- scripts/sql/create/create_cell_coords_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `codenome_db`.`cell_coords` (
-  `center` INT NOT NULL,
-  `v1` INT NOT NULL,
-  `v2` INT NOT NULL,
-  `v3` INT NOT NULL,
-  `v4` INT NOT NULL,
-  `v5` INT NOT NULL,
-  `v6` INT,
-  PRIMARY KEY (`center`));
