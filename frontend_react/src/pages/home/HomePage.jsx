bb26a0ce322f2f515cc4ac01b83737eab0a6da16
Bryce Schultz <bryceschultz@live.com>
2023-04-20 22:05:39 -0700

    switched to events (#90)

--- frontend_react/src/pages/home/HomePage.jsx
+++ frontend_react/src/pages/home/HomePage.jsx
@@ -12,12 +12,16 @@ import Routing from '../../components/routing_info/routing';
 import Directions from '../../components/directions/directions';
 import Map from '../../components/map/Map';
 import { hasLocalData, loadRoutes } from '../../utilities/offline_data/offline_data';
+import PinCreator from '../../components/pin_creator/PinCreator';
 
 import './home.css';
+import { useState } from 'react';
+import event from '../../utilities/event';
 
 // this function renders the home page of the application.
 export default function HomePage() 
 {
+  const [showPinCreator, setShowPinCreator] = useState(false);
   const pageLoad = () =>
   {
     loadUserData();
@@ -35,10 +39,21 @@ export default function HomePage()
     }
   }
 
+  event.on('show-pin-creator', () =>
+  {
+    setShowPinCreator(true);
+  });
+
+  event.on('close-pin-creator', () =>
+  {
+    setShowPinCreator(false);
+  });
+
   return (
     <>
       <div id='content' onLoad={pageLoad}>
         <Map/>
+        { showPinCreator && <PinCreator/> }
       </div>
       <Routing/>
       <Directions/>
