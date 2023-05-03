a5c32853c2b89ca8e8a342179e1c049f84303c80
Andrew Rice <aprspie@gmail.com>
2023-05-03 14:44:46 -0700

    Updated cell stuff

--- backend_api/GnomadAPI/Controllers/CellularController.cs
+++ backend_api/GnomadAPI/Controllers/CellularController.cs
@@ -29,59 +29,21 @@ namespace TravelCompanionAPI.Controllers
             _repo = repo;
         }
 
-        [HttpGet("saveToDatabase")]
-        public JsonResult saveData()
+        [HttpGet("saveData/{state}")]
+        public JsonResult saveData(string state)
         {
 
-            _repo.SaveToDatabase();
+            _repo.SaveToDatabase(state);
 
             return new JsonResult(Ok());
         }
 
-        [HttpGet("getById")]
-        public JsonResult get(string id)
+        [HttpGet("allH3/{state}")]
+        public JsonResult getAllH3(string state)
         {
-
-            Cellular cellular = _repo.getByH3Id(id);
-
-            if (cellular == null)
-            {
-                return new JsonResult(NotFound());
-            }
-
-            return new JsonResult(Ok(cellular));
-        }
-
-        [HttpGet("all")]
-        public JsonResult getAll()
-        {
-            List<Cellular> cellularList = _repo.getAll();
-
-            return new JsonResult(Ok(cellularList));
-        }
-
-        [HttpGet("allH3Id/{offset}")]
-        public JsonResult getAllH3()
-        {
-            List<string> h3List = _repo.getAllH3();
+            List<string> h3List = _repo.getAllH3(state);
 
             return new JsonResult(Ok(h3List));
-        }
-
-        [HttpGet("allInRange/{latMin}/{lngMin}/{latMax}/{lngMax}")]
-        public JsonResult getIdsInRange(float latMin, float lngMin, float latMax, float lngMax)
-        {
-            List<int> inRange = _repo.getIdsInRange(latMin, lngMin, latMax, lngMax);
-
-            return new JsonResult(Ok(inRange));
-        }
-
-        [HttpGet("allCoords/{latMin}/{lngMin}/{latMax}/{lngMax}")]
-        public (JsonResult, JsonResult) getAllCoords(float latMin, float lngMin, float latMax, float lngMax)
-        {
-            (List<float> latList, List<float> lngList) = _repo.getAllCoords(latMin, lngMin, latMax, lngMax);
-
-            return (new JsonResult(Ok(latList)), new JsonResult(Ok(lngList)));
         }
 
         [HttpGet("allCoordsSingle/{max_pass}/{latMin}/{lngMin}/{latMax}/{lngMax}")]
@@ -90,16 +52,6 @@ namespace TravelCompanionAPI.Controllers
             List<float> latLngList = _repo.getAllCoordsThreaded(max_pass, latMin, lngMin, latMax, lngMax);
 
             return new JsonResult(Ok(latLngList));
-        }
-
-        [HttpGet("allHexCoords/{pass}")]
-        public JsonResult getHexCoords(int pass)
-        {
-            List<decimal> h3List = _repo.getHexCoords(pass);
-
-            decimal[] h3Array = h3List.ToArray();
-
-            return new JsonResult(Ok(h3Array));
         }
     }
 }
