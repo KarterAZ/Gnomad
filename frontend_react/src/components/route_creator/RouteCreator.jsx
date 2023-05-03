0754e324a9204a2c7dd316dbc0a4b05adcc38d3d
Bryce Schultz <bryceschultz@live.com>
2023-05-03 14:25:26 -0700

    Fix routes creator (#100)
    * changed some css
    
    * made some changes suggested during code review
    
    * commented the search bar

--- frontend_react/src/components/route_creator/RouteCreator.jsx
+++ frontend_react/src/components/route_creator/RouteCreator.jsx
@@ -11,135 +11,136 @@ import './route_creator.css';
 // Purpose: creates a window to allow the user to create a route.
 export default function RouteCreator()
 {
-    // variable states.
-    const [routeName, setRouteName] = useState('');
-    const [routePins, setRoutePins] = useState([]);
+  // variable states.
+  const [routeName, setRouteName] = useState('');
+  const [routePins, setRoutePins] = useState([]);
 
-    const [pinResults, setPinResults] = useState([]);
+  const [pinResults, setPinResults] = useState([]);
 
-    // error states.
-    const [nameError, setNameError] = useState('');
-    const [pinsError, setPinsError] = useState('');
+  // error states.
+  const [nameError, setNameError] = useState('');
+  const [pinsError, setPinsError] = useState('');
 
-    // this function verifies that the input in each field is valid.
-    // if its not valid it sets the correct error state.
-    const validateInput = () =>
+  // this function verifies that the input in each field is valid.
+  // if its not valid it sets the correct error state.
+  const validateInput = () =>
+  {
+    let status = true;
+    // TODO: replace these with regex for whitespace.
+    if (routeName.trim().length === 0)
     {
-        // TODO: replace these with regex for whitespace.
-        if (routeName === '')
-        {
-        setNameError('This cannot be left blank.');
-        return false;
-        }
-
-        if (routePins.length <= 0)
-        {
-        setPinsError('You must have at least one pin.');
-        return false;
-        }
-
-        return true;
+      setNameError('This cannot be left blank.');
+      status = false;
     }
 
-    const submit = () =>
+    if (routePins.length <= 0)
     {
-        if (validateInput())
-        {
-            //TODO create route here.
-            close();
-        }
+      setPinsError('You must have at least one pin.');
+      status =  false;
     }
 
-    const close = () =>
+    return status;
+  }
+
+  const submit = () =>
+  {
+    if (validateInput())
     {
-        event.emit('close-route-creator');
+      // TODO: create route here.
+      close();
     }
+}
 
-    const search = async (searchQuery) =>
+  const close = () =>
+  {
+    event.emit('close-route-creator');
+  }
+
+  const search = async (searchQuery) =>
+  {
+    // remove whitespace from beginning and end of the query.
+    let query = searchQuery.trim();
+
+    // if the query is not empty...
+    if (query.length !== 0)
     {
-        // remove whitespace from beginning and end of the query.
-        let query = searchQuery.trim();
+      // get the pins matching the search from backend.
+      let pins = await searchPins(searchQuery);
 
-        // if the query is not empty...
-        if (query.length !== 0)
-        {
-            // get the pins matching the search from backend.
-            let pins = await searchPins(searchQuery);
-
-            // set the pins to a pin list.
-            setPinResults(pins.map((pin, index) => <ListPin key={index} pin={pin}/>));
-        }
+      // set the pins to a pin list.
+      setPinResults(pins.map((pin, index) => <ListPin key={index} pin={pin}/>));
     }
+  }
 
-    const addToRoute = (pin) =>
-    {
-        setRoutePins([...routePins, pin]);
-    }
+  const addToRoute = (pin) =>
+  {
+    setRoutePins(list => [...list, pin]);
+  }
 
-    const ListPin = ({pin}) =>
-    {
-        return (
-            <li>
-                <div className='result-pin'>
-                    <span className='add-pin-title'>{pin.title}</span>
-                    <button className='add-pin-button' onClick={() => addToRoute(pin)}>+</button>
-                </div>
-            </li>
-        );
-    }
-
-    // HTML for the pin creation window.
+  const ListPin = ({pin}) =>
+  {
     return (
-        <div id='route-creator'>
-            <div id='creator-header'>
-                <h2>Create a Route</h2>
-            </div>
-
-            <div id='creator-body'>
-                {/* section to enter route name */}
-                <div className='input-section'>
-                    <span id='input-label-wrapper'><label>Route Name</label> <label className='error'>{nameError}</label></span>
-                    <input 
-                        className='text-input' 
-                        type='text' 
-                        onChange={(event) => 
-                        {
-                            setRouteName(event.target.value); 
-                            setNameError('');
-                        }} 
-                    />
-                </div>
-
-                <div className='input-section' id='pin-description-input-wrapper'>
-                    <div id='route-pins-picker'>
-                        <div className='split-section' id='search-pins-list-wrapper'>
-                            <div>
-                            <label>Search for pins to add</label>
-                            <SearchBar onSubmit={search}/>
-                            </div>
-                            <div id='pins-search-results'>
-                            <ul id='search-pins-list'>
-                                {pinResults}
-                            </ul>
-                            </div>
-                        </div>
-                        <div className='split-section'>
-                            <label>Pins</label>
-                            <ul id='route-pins-list'>
-                            {routePins.map((pin, index) => (
-                                <li key={index}>{pin.title}</li>
-                            ))}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                
-                {/* section with the buttons */}
-                <div className='input-section row gap-10'>
-                    <button className='button' onClick={close}>Cancel</button>
-                    <button className='button' onClick={submit}>Create Route</button>
-                </div>
-            </div>
+      <li>
+        <div className='result-pin'>
+          <span className='add-pin-title'>{pin.title}</span>
+          <button className='add-pin-button' onClick={() => addToRoute(pin)}>+</button>
         </div>
+      </li>
     );
+  }
+
+  // HTML for the pin creation window.
+  return (
+      <div id='route-creator'>
+          <div id='creator-header'>
+              <h2>Create a Route</h2>
+          </div>
+
+          <div id='creator-body'>
+              {/* section to enter route name */}
+              <div className='input-section'>
+                  <span id='input-label-wrapper'><label>Route Name</label> <label className='error'>{nameError}</label></span>
+                  <input 
+                      className='text-input' 
+                      type='text' 
+                      onChange={(event) => 
+                      {
+                          setRouteName(event.target.value); 
+                          setNameError('');
+                      }} 
+                  />
+              </div>
+
+              <div className='input-section' id='pin-description-input-wrapper'>
+                  <div id='route-pins-picker'>
+                      <div className='split-section' id='search-pins-list-wrapper'>
+                          <div>
+                          <label>Search for pins to add</label>
+                          <SearchBar onSubmit={search}/>
+                          </div>
+                          <div id='pins-search-results'>
+                          <ul id='search-pins-list'>
+                              {pinResults}
+                          </ul>
+                          </div>
+                      </div>
+                      <div className='split-section'>
+                          <label>Pins</label>
+                          <ul id='route-pins-list'>
+                          {routePins.map((pin, index) => (
+                              <li key={index}>{pin.title}</li>
+                          ))}
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+              
+              {/* section with the buttons */}
+              <div className='input-section row gap-10'>
+                  <button className='button' onClick={close}>Cancel</button>
+                  <button className='button' onClick={submit}>Create Route</button>
+              </div>
+          </div>
+      </div>
+  );
 }
