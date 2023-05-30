a0f885589f618252e489b83943e5523d80ed48b9
KarterAZ <kazwetschke@gmail.com>
2023-05-30 14:14:59 -0700

    fixed various deleting functions

--- backend_api/GnomadAPI/Repositories/Interfaces/IPinRepository.cs
+++ backend_api/GnomadAPI/Repositories/Interfaces/IPinRepository.cs
@@ -46,8 +46,6 @@ namespace TravelCompanionAPI.Data
 
         public List<Pin> getStickers(int uid, double latStart = 0, double longStart = 0, double latRange = 0, double longRange = 0);
 
-        public bool removePin(Pin pin);
-
         public bool autoRemove(int pinId);
     }
 }
